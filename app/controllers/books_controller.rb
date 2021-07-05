6d01df98b7bd527d79092e40096ead465f1a2931
miharatoki <ec2-user@ip-172-31-20-206.ec2.internal>
2021-07-05 04:48:57 +0000

    バリデーションチェック以外完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
     @book = Book.new
   end
 
@@ -8,16 +9,40 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save #新規投稿が成功したら
+      flash[:notice] = "Book was successfully created." #サクセルメッセージを表示
+      redirect_to book_path(book.id) 
+    else 
+      @books = Book.all
+      @book = Book.new
+      render action: :index
+    end
   end
   
   def show
     @book = Book.find(params[:id])
-    
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully updated." 
+      redirect_to book_path(book.id)
+    else
+      render action: :edit
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+     redirect_to books_path
+    end
   end
   
   private
